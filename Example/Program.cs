e610f887308868dcd4ee17c9588205abb881c57d
Delfim Uqueio Jr <delfim.uu.junior@gmail.com>
2021-10-05 06:54:22 +0200

    rename: renamed the solution name

--- Example/Program.cs
+++ Example/Program.cs
@@ -3,15 +3,14 @@ using System.Threading.Tasks;
 using MPesa;
 using Environment = MPesa.Environment;
 
-namespace Caller
+namespace Example
 {
     class Program
     {
         private static async Task Main(string[] args)
         {
             const string apiKey = "";
-            const string publicKey =
-                "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAmptSWqV7cGUUJJhUBxsMLonux24u+FoTlrb+4Kgc6092JIszmI1QUoMohaDDXSVueXx6IXwYGsjjWY32HGXj1iQhkALXfObJ4DqXn5h6E8y5/xQYNAyd5bpN5Z8r892B6toGzZQVB7qtebH4apDjmvTi5FGZVjVYxalyyQkj4uQbbRQjgCkubSi45Xl4CGtLqZztsKssWz3mcKncgTnq3DHGYYEYiKq0xIj100LGbnvNz20Sgqmw/cH+Bua4GJsWYLEqf/h/yiMgiBbxFxsnwZl0im5vXDlwKPw+QnO2fscDhxZFAwV06bgG0oEoWm9FnjMsfvwm0rUNYFlZ+TOtCEhmhtFp+Tsx9jPCuOd5h2emGdSKD8A6jtwhNa7oQ8RtLEEqwAn44orENa1ibOkxMiiiFpmmJkwgZPOG/zMCjXIrrhDWTDUOZaPx/lEQoInJoE2i43VN/HTGCCw8dKQAwg0jsEXau5ixD0GUothqvuX3B9taoeoFAIvUPEq35YulprMM7ThdKodSHvhnwKG82dCsodRwY428kg2xM/UjiTENog4B6zzZfPhMxFlOSFX4MnrqkAS+8Jamhy1GgoHkEMrsT5+/ofjCx0HjKbT5NuA2V/lmzgJLl3jIERadLzuTYnKGWxVJcGLkWXlEPYLbiaKzbJb2sYxt+Kt5OxQqC1MCAwEAAQ==";
+            const string publicKey = "";
 
             var client = new Client.Builder()
                 .ApiKey(apiKey)
@@ -34,6 +33,14 @@ namespace Caller
             try
             {
                 var response = await client.Receive(paymentRequest);
+                if (response.IsSuccessfully)
+                {
+                    Console.WriteLine(response);
+                }
+                else
+                {
+                    throw new Exception(response.Description);
+                }
             }
             catch (Exception e)
             {
